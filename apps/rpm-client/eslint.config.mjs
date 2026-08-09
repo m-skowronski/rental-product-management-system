ead770a0e3c32aadd31f934c04efa11bbb04513b
m-skowronski <mskowronski547@gmail.com>
2026-08-09 22:46:46 +0200

    build: configure eslint & husky
    #RPM-2

--- apps/rpm-client/eslint.config.mjs
+++ apps/rpm-client/eslint.config.mjs
@@ -1,38 +1,55 @@
-import nx from "@nx/eslint-plugin";
-import baseConfig from "../../eslint.config.mjs";
+import eslint from '@eslint/js';
+import nx from '@nx/eslint-plugin';
+import angular from 'angular-eslint';
+import tseslint from 'typescript-eslint';
+import ngrx from '@ngrx/eslint-plugin/v9';
+import baseConfig from '../../eslint.config.mjs';
 
-export default [
-    ...nx.configs["flat/angular"],
-    ...nx.configs["flat/angular-template"],
-    ...baseConfig,
-    {
-        files: [
-            "**/*.ts"
-        ],
-        rules: {
-            "@angular-eslint/directive-selector": [
-                "error",
-                {
-                    type: "attribute",
-                    prefix: "app",
-                    style: "camelCase"
-                }
-            ],
-            "@angular-eslint/component-selector": [
-                "error",
-                {
-                    type: "element",
-                    prefix: "app",
-                    style: "kebab-case"
-                }
-            ]
-        }
+export default tseslint.config(
+  ...nx.configs['flat/angular'],
+  ...nx.configs['flat/angular-template'],
+  ...baseConfig,
+  {
+    files: ['**/*.ts'],
+    extends: [
+      eslint.configs.recommended,
+      ...tseslint.configs.recommendedTypeChecked,
+      ...tseslint.configs.stylisticTypeChecked,
+      ...angular.configs.tsRecommended,
+      ...ngrx.configs.signalsTypeChecked,
+    ],
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
     },
-    {
-        files: [
-            "**/*.html"
-        ],
-        // Override or add rules here
-        rules: {}
-    }
-];
+    rules: {
+      '@angular-eslint/directive-selector': [
+        'error',
+        {
+          type: 'attribute',
+          prefix: 'app',
+          style: 'camelCase',
+        },
+      ],
+      '@angular-eslint/component-selector': [
+        'error',
+        {
+          type: 'element',
+          prefix: 'app',
+          style: 'kebab-case',
+        },
+      ],
+    },
+  },
+  {
+    files: ['**/*.html'],
+    extends: [
+      ...angular.configs.templateRecommended,
+      ...angular.configs.templateAccessibility,
+    ],
+    // Override or add rules here
+    rules: {},
+  },
+);
