c775270ef4980426c90413d704afbf354fdb747f
m-skowronski <mskowronski547@gmail.com>
2026-08-10 19:19:39 +0200

    build: add transloco dependency
    #RPM-2

--- apps/rpm-client/src/app/app.config.ts
+++ apps/rpm-client/src/app/app.config.ts
@@ -1,10 +1,27 @@
+import { provideHttpClient } from '@angular/common/http';
 import {
   ApplicationConfig,
+  isDevMode,
   provideBrowserGlobalErrorListeners,
 } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideTransloco } from '@jsverse/transloco';
 import { appRoutes } from './app.routes';
+import { TranslocoHttpLoader } from './transloco-loader';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideBrowserGlobalErrorListeners(), provideRouter(appRoutes)],
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideRouter(appRoutes),
+    provideHttpClient(),
+    provideTransloco({
+      config: {
+        availableLangs: ['en', 'pl', 'uk'],
+        defaultLang: 'pl',
+        reRenderOnLangChange: true,
+        prodMode: !isDevMode(),
+      },
+      loader: TranslocoHttpLoader,
+    }),
+  ],
 };
